f7edde77661eedcfe64c4cea1a32690dbd5548ba
Siddharth <siddhary87@gmail.com>
2023-07-07 17:14:35 +0530

    latest checkin local

--- demo/src/main/java/com/example/demo/entity/GuestType.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.entity;
-
-
-public enum GuestType {
-    FIRST,
-    BUSINESS,
-    ECONOMY
-
-}
--- demo/src/main/java/com/example/demo/repository/EdgeListRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.EdgeList;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EdgeListRepo extends CrudRepository<EdgeList, String> {
-}
